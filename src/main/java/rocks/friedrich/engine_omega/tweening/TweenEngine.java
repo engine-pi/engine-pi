ae8965a31ab7ebfad0206069b39942c563c88531
Josef Friedrich <josef@friedrich.rocks>
2024-05-13 22:40:13 +0200

    Add the tween engine from the litiengine

---
+++ src/main/java/rocks/friedrich/engine_omega/tweening/TweenEngine.java
@@ -0,0 +1,235 @@
+/*
+ * Source: https://github.com/gurkenlabs/litiengine/blob/main/litiengine/src/main/java/de/gurkenlabs/litiengine/tweening/TweenEngine.java
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016 - 2024 Gurkenlabs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package rocks.friedrich.engine_omega.tweening;
+
+import rocks.friedrich.engine_omega.Game;
+import rocks.friedrich.engine_omega.event.FrameUpdateListener;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * The TweenEngine is the central manager for Tweens. It tracks all current
+ * Tween instances and applies their {@code TweenEquation} with every
+ * {@code GameLoop} update.
+ */
+public class TweenEngine implements FrameUpdateListener
+{
+    private final Map<Tweenable, Map<TweenType, Tween>> tweens;
+
+    /**
+     * Instantiates a new {@code TweenEngine}.
+     */
+    public TweenEngine()
+    {
+        this.tweens = new ConcurrentHashMap<>();
+    }
+
+    /**
+     * Begins a new Tween. If a Tween is already registered for the
+     * {@code Tweenable} with the given {@code TweenType}, it is restarted with
+     * the given duration.
+     *
+     * @param target   the {@code Tweenable} target object
+     * @param type     the {@code TweenType} determining which values of the
+     *                 target object will be modified.
+     * @param duration the duration of the Tween in milliseconds.
+     * @return the Tween instance
+     */
+    public Tween begin(final Tweenable target, final TweenType type,
+            final int duration)
+    {
+        Tween tween = this.getTween(target, type);
+        if (tween == null)
+        {
+            tween = new Tween(target, type, duration)
+                    .ease(TweenFunction.QUAD_INOUT);
+            this.getTweens().get(target).put(type, tween);
+        }
+        else
+        {
+            tween.setDuration(duration);
+        }
+        tween.begin();
+        return tween;
+    }
+
+    /**
+     * Attempts to get a previously registered {@code Tween} or registers a new
+     * one.
+     *
+     * @param target the {@code Tweenable} target object
+     * @param type   the {@code TweenType} determining which values of the
+     *               target object will be modified.
+     * @return the Tween instance
+     */
+    public Tween getTween(final Tweenable target, final TweenType type)
+    {
+        if (this.getTweens().get(target) == null)
+        {
+            this.getTweens().put(target, new ConcurrentHashMap<>());
+        }
+        return this.getTweens().get(target).get(type);
+    }
+
+    /**
+     * Gets the map of registered {@code Tweens}.
+     *
+     * @return the map of registered {@code Tweens}.
+     */
+    public Map<Tweenable, Map<TweenType, Tween>> getTweens()
+    {
+        return this.tweens;
+    }
+
+    /**
+     * Looks for a registered Tween instance with the given target and type.
+     * Attempts to stop the Tween and reset the {@code Tweenable} values to the
+     * start values.
+     *
+     * @param target the {@code Tweenable} target object
+     * @param type   the {@code TweenType} determining which values of the
+     *               target object will be modified.
+     * @return the Tween instance
+     */
+    public Tween reset(final Tweenable target, final TweenType type)
+    {
+        final Tween tween = this.getTween(target, type);
+        if (tween != null)
+        {
+            tween.stop();
+            tween.reset();
+        }
+        return tween;
+    }
+
+    /**
+     * Looks for a registered Tween instance with the given target and type.
+     * Attempts to resume the Tween if it was stopped.
+     *
+     * @param target the {@code Tweenable} target object
+     * @param type   the {@code TweenType} determining which values of the
+     *               target object will be modified.
+     * @return the Tween instance
+     */
+    public Tween resume(final Tweenable target, final TweenType type)
+    {
+        final Tween tween = this.getTween(target, type);
+        if (tween != null)
+        {
+            tween.resume();
+        }
+        return tween;
+    }
+
+    /**
+     * Start.
+     */
+    public void start()
+    {
+        Game.addFrameUpdateListener(this);
+    }
+
+    /**
+     * Looks for a registered Tween instance with the given target and type.
+     * Attempts to remove the Tween from the TweenEngine.
+     *
+     * @param target the {@code Tweenable} target object
+     * @param type   the {@code TweenType} determining which values of the
+     *               target object will be modified.
+     */
+    public void remove(final Tweenable target, final TweenType type)
+    {
+        final Tween tween = this.getTween(target, type);
+        if (tween != null)
+        {
+            this.getTweens().get(target).remove(type);
+        }
+    }
+
+    /**
+     * Looks for a registered Tween instance with the given target and type.
+     * Attempts to stop the Tween.
+     *
+     * @param target the {@code Tweenable} target object
+     * @param type   the {@code TweenType} determining which values of the
+     *               target object will be modified.
+     * @return the Tween instance
+     */
+    public Tween stop(final Tweenable target, final TweenType type)
+    {
+        final Tween tween = this.getTween(target, type);
+        if (tween != null)
+        {
+            tween.stop();
+        }
+        return tween;
+    }
+
+    /**
+     * Terminate.
+     */
+    public void terminate()
+    {
+        Game.removeFrameUpdateListener(this);
+    }
+
+    /**
+     * Updates all registered Tweens by applying the {@code TweenEquation}.
+     */
+    @Override
+    public void onFrameUpdate(double deltaSeconds)
+    {
+        for (final Tweenable target : this.getTweens().keySet())
+        {
+            for (final Tween tween : this.getTweens().get(target).values())
+            {
+                if (tween.hasStopped())
+                {
+                    continue;
+                }
+                final long elapsed = tween.getStartTime();
+                if (elapsed >= tween.getDuration())
+                {
+                    tween.stop();
+                    continue;
+                }
+                final float[] currentValues = new float[tween
+                        .getTargetValues().length];
+                for (int i = 0; i < tween.getTargetValues().length; i++)
+                {
+                    currentValues[i] = tween.getStartValues()[i]
+                            + tween.getEquation().compute(
+                                    elapsed / (float) tween.getDuration())
+                                    * (tween.getTargetValues()[i]
+                                            - tween.getStartValues()[i]);
+                }
+                tween.getTarget().setTweenValues(tween.getType(),
+                        currentValues);
+            }
+        }
+    }
+}
---
+++ src/main/java/rocks/friedrich/engine_omega/tweening/TweenFunction.java
@@ -0,0 +1,154 @@
+/*
+ * Source: https://github.com/gurkenlabs/litiengine/blob/main/litiengine/src/main/java/de/gurkenlabs/litiengine/tweening/TweenFunction.java
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016 - 2024 Gurkenlabs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package rocks.friedrich.engine_omega.tweening;
+
+/**
+ * Easing equations based on Robert Penner's work:
+ * http://robertpenner.com/easing/
+ */
+public enum TweenFunction
+{
+    LINEAR(time -> time),
+    // Quad based (x^2)
+    QUAD_IN(time -> (float) Math.pow(time, 2)),
+    QUAD_OUT(time -> (float) (1f - Math.pow((time - 1f), 2))),
+    QUAD_INOUT(time -> (float) (time * 2f < 1f ? Math.pow(time, 2) * 2f
+            : 1f - Math.pow(time - 1f, 2) * 2f)),
+    // Circle
+    CIRCLE_IN(time -> (float) (1.0F - Math.sqrt(1.0F - Math.pow(time, 2)))),
+    CIRCLE_OUT(time -> (float) Math.sqrt(1.0F - Math.pow(time - 1f, 2))),
+    CIRCLE_INOUT(time -> (float) (time * 2f < 1f
+            ? (1f - Math.sqrt(1.0F - Math.pow(time * 2f, 2))) * 0.5
+            : (Math.sqrt(1f - 4 * Math.pow((time - 1f), 2)) + 1.0f) * 0.5f)),
+    // Sine
+    SINE_IN(time -> (float) (1.0f - Math.cos(time * Math.PI / 2f))),
+    SINE_OUT(time -> (float) (1.0f - Math.sin(time * Math.PI / 2f))),
+    SINE_INOUT(time -> (float) (.5f * (1f - Math.cos(time * Math.PI / 2f)))),
+    // Exponential
+    EXPO_IN(time -> (float) Math.pow(2, 10f * (time - 1f))),
+    EXPO_OUT(time -> (float) (1f - Math.pow(2, -10f * time))),
+    EXPO_INOUT(time -> (float) (time < .5f
+            ? Math.pow(2, 10f * (2f * time - 1f) - 1f)
+            : 1f - Math.pow(2, -10f * (2f * time - 1f) - 1f))),
+    // Back
+    BACK_IN(time -> (float) Math.pow(time, 2)
+            * (time * (1.70158f + 1f) - 1.70158f)),
+    BACK_OUT(time -> {
+        final float k = 1.70158F;
+        return (float) (1f
+                + Math.pow(time - 1f, 2) * ((time - 1f) * (k + 1f) + k));
+    }), BACK_INOUT(time -> {
+        final float k2 = 1.70158F * 1.525F;
+        return (float) (time < .5f
+                ? Math.pow(time, 2) * 2.0F * (time * 2.0F * (k2 + 1.0F) - k2)
+                : 1.0F + 2.0F * Math.pow(time - 1f, 2)
+                        * (2.0F * (time - 1.0F) * (k2 + 1.0F) + k2));
+    }),
+    // Bounce
+    BOUNCE_OUT(time -> {
+        final float BOUNCE_R = 1.0F / 2.75F; // reciprocal
+        final float BOUNCE_K0 = 7.5625F;
+        final float BOUNCE_K1 = 1.0F * BOUNCE_R; // 36.36%
+        final float BOUNCE_K2 = 2.0F * BOUNCE_R; // 72.72%
+        final float BOUNCE_K3 = 1.5F * BOUNCE_R; // 54.54%
+        final float BOUNCE_K4 = 2.5F * BOUNCE_R; // 90.90%
+        final float BOUNCE_K5 = 2.25F * BOUNCE_R; // 81.81%
+        final float BOUNCE_K6 = 2.625F * BOUNCE_R; // 95.45%
+        if (time < BOUNCE_K1)
+        {
+            return BOUNCE_K0 * time * time;
+        }
+        else if (time < BOUNCE_K2)
+        {
+            // 48/64
+            final float t = time - BOUNCE_K3;
+            return BOUNCE_K0 * t * t + 0.75F;
+        }
+        else if (time < BOUNCE_K4)
+        {
+            // 60/64
+            final float t = time - BOUNCE_K5;
+            return BOUNCE_K0 * t * t + 0.9375F;
+        }
+        else
+        {
+            // 63/64
+            final float t = time - BOUNCE_K6;
+            return BOUNCE_K0 * t * t + 0.984375F;
+        }
+    }), BOUNCE_IN(time -> 1f - BOUNCE_OUT.equation.compute(1f - time)),
+    BOUNCE_INOUT(time -> time * 2f < 1f
+            ? 0.5F - 0.5F * BOUNCE_OUT.equation.compute(1.0F - time * 2)
+            : 0.5F + 0.5F * BOUNCE_OUT.equation.compute(time * 2 - 1.0F)),
+    // Elastic
+    ELASTIC_IN(time -> (float) (-Math.pow(2, 10f * (time - 1f))
+            * Math.sin(((time - 1f) * 40f - 3f) * Math.PI / 6f))),
+    ELASTIC_OUT(time -> (float) (1f + (Math.pow(2, 10f * -time)
+            * Math.sin((-time * 40f - 3f) * Math.PI / 6f)))),
+    ELASTIC_INOUT(time -> {
+        time *= 2.0F; // remap: [0,0.5] -> [-1,0]
+        time -= 1.0F; // and [0.5,1] -> [0,+1]
+        final float k = (float) ((80f * time - 9f) * Math.PI / 18f);
+        if (time < 0.0F)
+        {
+            return (float) (-.5f * Math.pow(2, 10f * time) * Math.sin(k));
+        }
+        return (float) (1f + .5F * Math.pow(2, -10f * time) * Math.sin(k));
+    });
+
+    private final transient TweenEquation equation;
+
+    /**
+     * Instantiates a new tween function with a given mathematical equation.
+     *
+     * @param equation the equation
+     */
+    TweenFunction(final TweenEquation equation)
+    {
+        this.equation = equation;
+    }
+
+    /**
+     * Gets the mathematical equation.
+     *
+     * @return the equation
+     */
+    public TweenEquation getEquation()
+    {
+        return equation;
+    }
+
+    /**
+     * Computes the next value of the interpolation.
+     *
+     * @param time The current progress of the tween duration, between 0 and 1.
+     * @return The next interpolated value.
+     */
+    public float compute(float time)
+    {
+        return equation.compute(time);
+    }
+}
