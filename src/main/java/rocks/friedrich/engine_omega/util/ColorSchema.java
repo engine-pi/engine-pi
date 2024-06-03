22823d28cab4d001822ee7125171e99a077fdc7a
Josef Friedrich <josef@friedrich.rocks>
2024-06-03 21:31:36 +0200

    Add color schema

---
+++ src/main/java/rocks/friedrich/engine_omega/util/ColorSchema.java
@@ -0,0 +1,69 @@
+package rocks.friedrich.engine_omega.util;
+
+import java.awt.Color;
+
+/**
+ * https://developer.gnome.org/hig/reference/palette.html
+ */
+public class ColorSchema
+{
+    /**
+     * green3
+     */
+    Color green = new Color(51, 209, 122);
+
+    /**
+     * light1
+     */
+    Color white = Color.WHITE;
+
+    /**
+     * orange3
+     */
+    Color orange = new Color(255, 120, 0);
+
+    /**
+     * light5
+     */
+    Color gray = new Color(154, 153, 150);
+
+    /**
+     * blue3
+     */
+    Color blue = new Color(53, 132, 228);
+
+    /**
+     * red3
+     */
+    Color red = new Color(224, 27, 36);
+
+    /**
+     * https://de.wikipedia.org/wiki/Pink_(Farbe) purple1
+     */
+    Color pink = new Color(220, 138, 221);
+
+    /**
+     * https://de.wikipedia.org/wiki/Magenta_(Farbe) purple3
+     */
+    Color magenta = new Color(97, 53, 131);
+
+    /**
+     * https://de.wikipedia.org/wiki/T%C3%BCrkis_(Farbe)
+     */
+    Color cyan;
+
+    /**
+     * https://de.wikipedia.org/wiki/Violett lila purple3
+     */
+    Color violet = new Color(145, 65, 172);
+
+    /**
+     * brown3
+     */
+    Color brown = new Color(152, 106, 68);
+
+    /**
+     * dark5
+     */
+    Color black = new Color(0, 0, 0);
+}
