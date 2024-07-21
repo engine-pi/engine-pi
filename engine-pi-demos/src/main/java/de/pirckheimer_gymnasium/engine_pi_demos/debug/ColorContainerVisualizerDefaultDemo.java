1652cc830a004669f93e71573245ad599bd29b1a
Josef Friedrich <josef@friedrich.rocks>
2024-07-21 12:07:22 +0200

    Improve javadoc

---
+++ engine-pi-demos/src/main/java/de/pirckheimer_gymnasium/engine_pi_demos/debug/ColorContainerVisualizerDefaultDemo.java
@@ -0,0 +1,30 @@
+package de.pirckheimer_gymnasium.engine_pi_demos.debug;
+
+import de.pirckheimer_gymnasium.engine_pi.Game;
+import de.pirckheimer_gymnasium.engine_pi.Resources;
+import de.pirckheimer_gymnasium.engine_pi.Scene;
+import de.pirckheimer_gymnasium.engine_pi.debug.ColorContainerVisualizer;
+
+/**
+ * Demonstriert die Klasse {@link ColorContainerVisualizer} mit dem
+ * <b>Standard-Farben-Speicher</b>.
+ *
+ * <p>
+ * <img alt="ColorContainerVisualizerDefault" src=
+ * "https://raw.githubusercontent.com/engine-pi/engine-pi/main/misc/images/debug/ColorContainerVisualizerDefault.png">
+ * </p>
+ *
+ * @author Josef Friedrich
+ */
+public class ColorContainerVisualizerDefaultDemo
+{
+    public static void main(String[] args)
+    {
+        Game.start(new Scene()
+        {
+            {
+                new ColorContainerVisualizer(this);
+            }
+        });
+    }
+}
--- engine-pi/src/main/java/de/pirckheimer_gymnasium/engine_pi/debug/package-info.java
+++ engine-pi/src/main/java/de/pirckheimer_gymnasium/engine_pi/debug/package-info.java
@@ -1,5 +1,4 @@
 /**
  * Liefert Klassen zum <b>Zeichnen</b> von <b>Debug-Informationen</b>.
- *
  */
 package de.pirckheimer_gymnasium.engine_pi.debug;
