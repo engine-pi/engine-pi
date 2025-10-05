a48d7b24139a15c58547f14aae2926b1ab304b1c
Josef Friedrich <josef@friedrich.rocks>
2025-10-05 08:10:56 +0200

    Bump version 0.32.0

--- engine-pi-demos/src/main/java/de/pirckheimer_gymnasium/engine_pi_demos/little_engine/turtle/LevyCCurve.java
+++ engine-pi-demos/src/main/java/de/pirckheimer_gymnasium/engine_pi_demos/little_engine/turtle/LevyCCurve.java
@@ -88,7 +88,7 @@ public class LevyCCurve
         if (depth > 0)
         {
             t.rotate(45);
-            //  1 / Math.sqrt(2) == 0.7071067811865475
+            // 1 / Math.sqrt(2) == 0.7071067811865475
             // sqrt -> square root -> Quadratwurzel
             drawPart(length * 0.7071, depth - 1);
             t.rotate(-90);
