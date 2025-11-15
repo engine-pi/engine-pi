d6ed23780e92c948e31646bfc6520ee153610eaa
Josef Friedrich <josef@friedrich.rocks>
2025-11-15 17:38:12 +0100

    Implement an interface to control the turtle

--- modules/engine/src/main/java/de/pirckheimer_gymnasium/engine_pi/dsa/turtle/Turtle.java
+++ modules/engine/src/main/java/de/pirckheimer_gymnasium/engine_pi/dsa/turtle/Turtle.java
@@ -92,7 +92,7 @@ import de.pirckheimer_gymnasium.engine_pi.util.ColorUtil;
  * @author Michael Andonie
  * @author Niklas Keller
  */
-public class Turtle extends PaintingSurfaceScene
+public class Turtle extends PaintingSurfaceScene implements TurtleControl
 {
     /**
      * Die <b>graphische Repräsentation</b> der Schildkröte.
@@ -166,29 +166,11 @@ public class Turtle extends PaintingSurfaceScene
 
     /* Hauptmethoden */
 
-    /**
-     * <b>Bewegt</b> die Schildkröte in Blickrichtung 3 Meter nach vorne.
-     *
-     * @since 0.38.0
-     */
     public void move()
     {
         move(3);
     }
 
-    /**
-     * <b>Bewegt</b> die Schildkröte in Blickrichtung nach vorne.
-     *
-     * <p>
-     * In der GraphicsAndGames-Engine des Cornelsen Verlags heißt die Methode
-     * {@code Gehen}, in der Engine Alpha {@code laufe}.
-     * </p>
-     *
-     * @param distance Die <b>Entfernung</b> in Meter, die die Schildkröte
-     *     zurücklegen soll.
-     *
-     * @since 0.38.0
-     */
     public void move(double distance)
     {
         // Vielleicht wäre es besser die Rotation auch extra zu speichern wie
@@ -242,19 +224,6 @@ public class Turtle extends PaintingSurfaceScene
         turtleImage.setPosition(pen.position);
     }
 
-    /**
-     * <b>Dreht</b> die Schildkröte.
-     *
-     * <p>
-     * In der GraphicsAndGames-Engine des Cornelsen Verlags heißt die Methode
-     * {@code Drehen}, in der Engine Alpha {@code rotiere}.
-     * </p>
-     *
-     * @param rotation Der <b>Drehwinkel</b> in Grad. Positive Werte drehen
-     *     gegen den Uhrzeigersinn, negative Werte im Uhrzeigersinn.
-     *
-     * @since 0.38.0
-     */
     public void rotate(double rotation)
     {
         double start = pen.direction;
@@ -272,31 +241,11 @@ public class Turtle extends PaintingSurfaceScene
         }
     }
 
-    /**
-     * <b>Wechselt</b> in den Modus <em>„zeichnen“</em>.
-     *
-     * <p>
-     * In der GraphicsAndGames-Engine des Cornelsen Verlags heißt die Methode
-     * {@code StiftSenken}, in der Engine Alpha {@code ansetzen}.
-     * </p>
-     *
-     * @since 0.38.0
-     */
     public void lowerPen()
     {
         pen.isDown = true;
     }
 
-    /**
-     * <b>Wechselt</b> in den Modus <em>„nicht zeichnen“</em>.
-     *
-     * <p>
-     * In der GraphicsAndGames-Engine des Cornelsen Verlags heißt die Methode
-     * {@code StiftHeben}, in der Engine Alpha {@code absetzen}.
-     * </p>
-     *
-     * @since 0.38.0
-     */
     public void liftPen()
     {
         pen.isDown = false;
@@ -449,21 +398,6 @@ public class Turtle extends PaintingSurfaceScene
         pen.color = colors.get(lineColor);
     }
 
-    /**
-     * Setzt die Schildkröte auf eine neue <b>Position</b>.
-     *
-     * <p>
-     * Im Gegensatz zur {@link #move(double)}-Methode geschieht die Bewegung
-     * hier ruckhaft. Die Schildkröte wird quasi in die Luft gehoben und an
-     * einer anderen Stelle wieder abgesetzt. Deshalb wird auch keine Linie
-     * gezeichnet und auch keine Animation durchgeführt. Sonst könnte mit dieser
-     * Methode „geschummelt“ werden.
-     * </p>
-     *
-     * @param position Die gewünschte <b>Position</b> als Vektor in Meter.
-     *
-     * @since 0.38.0
-     */
     public void setPosition(Vector position)
     {
         lastPosition = pen.position;
@@ -471,36 +405,11 @@ public class Turtle extends PaintingSurfaceScene
         turtleImage.setPosition(position);
     }
 
-    /**
-     * Setzt die Schildkröte auf eine neue <b>Position</b>.
-     *
-     * <p>
-     * Im Gegensatz zur {@link #move(double)}-Methode geschieht die Bewegung
-     * hier ruckhaft. Die Schildkröte wird quasi in die Luft gehoben und an
-     * einer anderen Stelle wieder abgesetzt. Deshalb wird auch keine Linie
-     * gezeichnet und auch keine Animation durchgeführt. Sonst könnte mit dieser
-     * Methode „geschummelt“ werden.
-     * </p>
-     *
-     * @param x Die <b>x</b>-Koordinate der gewünschten Position in Meter.
-     * @param y Die <b>y</b>-Koordinate der gewünschten Position in Meter.
-     *
-     * @since 0.38.0
-     */
     public void setPosition(double x, double y)
     {
         setPosition(new Vector(x, y));
     }
 
-    /**
-     * Setzt die <b>Blickrichtung</b> der Schildkröte.
-     *
-     * @param direction Die Blickrichtung der Schildkröte in Grad: 0°: nach
-     *     rechts (Osten), 90°: nach oben (Norden) 180°: nach links (Westen)
-     *     270°: nach unten (Süden)
-     *
-     * @since 0.38.0
-     */
     public void setDirection(double direction)
     {
         setCurrentRotation(direction);
---
+++ modules/engine/src/main/java/de/pirckheimer_gymnasium/engine_pi/dsa/turtle/TurtleControl.java
@@ -0,0 +1,121 @@
+package de.pirckheimer_gymnasium.engine_pi.dsa.turtle;
+
+import de.pirckheimer_gymnasium.engine_pi.Vector;
+
+/**
+ * Definiert die Methoden mit denen die Schildkröte gesteuert werden kann.
+ *
+ * Es handelt sich um das absolute Minimun an Methoden um eine Turtlegrafik
+ * zeichnen zu können. Methoden, die grafische Einstellungen vornehmen sind
+ * nicht enhalten.
+ */
+public interface TurtleControl
+{
+    /**
+     * <b>Bewegt</b> die Schildkröte in Blickrichtung 3 Meter nach vorne.
+     *
+     * @since 0.38.0
+     */
+    public void move();
+
+    /**
+     * <b>Bewegt</b> die Schildkröte in Blickrichtung nach vorne.
+     *
+     * <p>
+     * In der GraphicsAndGames-Engine des Cornelsen Verlags heißt die Methode
+     * {@code Gehen}, in der Engine Alpha {@code laufe}.
+     * </p>
+     *
+     * @param distance Die <b>Entfernung</b> in Meter, die die Schildkröte
+     *     zurücklegen soll.
+     *
+     * @since 0.38.0
+     */
+    public void move(double distance);
+
+    /**
+     * Setzt die Schildkröte auf eine neue <b>Position</b>.
+     *
+     * <p>
+     * Im Gegensatz zur {@link #move(double)}-Methode geschieht die Bewegung
+     * hier ruckhaft. Die Schildkröte wird quasi in die Luft gehoben und an
+     * einer anderen Stelle wieder abgesetzt. Deshalb wird auch keine Linie
+     * gezeichnet und auch keine Animation durchgeführt. Sonst könnte mit dieser
+     * Methode „geschummelt“ werden.
+     * </p>
+     *
+     * @param position Die gewünschte <b>Position</b> als Vektor in Meter.
+     *
+     * @since 0.38.0
+     */
+    void setPosition(Vector position);
+
+    /**
+     * Setzt die Schildkröte auf eine neue <b>Position</b>.
+     *
+     * <p>
+     * Im Gegensatz zur {@link #move(double)}-Methode geschieht die Bewegung
+     * hier ruckhaft. Die Schildkröte wird quasi in die Luft gehoben und an
+     * einer anderen Stelle wieder abgesetzt. Deshalb wird auch keine Linie
+     * gezeichnet und auch keine Animation durchgeführt. Sonst könnte mit dieser
+     * Methode „geschummelt“ werden.
+     * </p>
+     *
+     * @param x Die <b>x</b>-Koordinate der gewünschten Position in Meter.
+     * @param y Die <b>y</b>-Koordinate der gewünschten Position in Meter.
+     *
+     * @since 0.38.0
+     */
+    void setPosition(double x, double y);
+
+    /**
+     * <b>Dreht</b> die Schildkröte.
+     *
+     * <p>
+     * In der GraphicsAndGames-Engine des Cornelsen Verlags heißt die Methode
+     * {@code Drehen}, in der Engine Alpha {@code rotiere}.
+     * </p>
+     *
+     * @param rotation Der <b>Drehwinkel</b> in Grad. Positive Werte drehen
+     *     gegen den Uhrzeigersinn, negative Werte im Uhrzeigersinn.
+     *
+     * @since 0.38.0
+     */
+    public void rotate(double rotation);
+
+    /**
+     * Setzt die <b>Blickrichtung</b> der Schildkröte.
+     *
+     * @param direction Die Blickrichtung der Schildkröte in Grad: 0°: nach
+     *     rechts (Osten), 90°: nach oben (Norden) 180°: nach links (Westen)
+     *     270°: nach unten (Süden)
+     *
+     * @since 0.38.0
+     */
+    void setDirection(double direction);
+
+    /**
+     * <b>Wechselt</b> in den Modus <em>„zeichnen“</em>.
+     *
+     * <p>
+     * In der GraphicsAndGames-Engine des Cornelsen Verlags heißt die Methode
+     * {@code StiftSenken}, in der Engine Alpha {@code ansetzen}.
+     * </p>
+     *
+     * @since 0.38.0
+     */
+    public void lowerPen();
+
+    /**
+     * <b>Wechselt</b> in den Modus <em>„nicht zeichnen“</em>.
+     *
+     * <p>
+     * In der GraphicsAndGames-Engine des Cornelsen Verlags heißt die Methode
+     * {@code StiftHeben}, in der Engine Alpha {@code absetzen}.
+     * </p>
+     *
+     * @since 0.38.0
+     */
+    public void liftPen();
+
+}
