3484e5adf38d4de2b4665dd55e2290944ca2c4ea
Josef Friedrich <josef@friedrich.rocks>
2025-11-20 19:45:54 +0100

    Implement getRow() and getColumn()

--- modules/engine/src/main/java/de/pirckheimer_gymnasium/engine_pi/graphics/boxes/GridBox.java
+++ modules/engine/src/main/java/de/pirckheimer_gymnasium/engine_pi/graphics/boxes/GridBox.java
@@ -1,16 +1,62 @@
 package de.pirckheimer_gymnasium.engine_pi.graphics.boxes;
 
+import java.util.ArrayList;
+
 public class GridBox extends ChildsBox
 {
-    int numberOfColumns = 2;
+    int columns = 2;
 
     public GridBox(Box... childs)
     {
         super(childs);
     }
 
-    public int numberOfRows()
+    public GridBox columns(int columns)
     {
-        return (int) Math.ceil((double) numberOfChilds() / numberOfColumns);
+        this.columns = columns;
+        return this;
+    }
+
+    /**
+     * Gibt die Anzahl der Spalten zurück.
+     *
+     * @return Die Anzahl der Spalten.
+     */
+    public int columns()
+    {
+        return columns;
+    }
+
+    /**
+     * Gibt die Anzahl der Reihen zurück.
+     *
+     * @return Die Anzahl der Reihen.
+     */
+    public int rows()
+    {
+        return (int) Math.ceil((double) numberOfChilds() / columns);
+    }
+
+    public ArrayList<Box> getRow(int row)
+    {
+        // Anfangsindex
+        final int start = row * columns;
+        ArrayList<Box> rowChilds = new ArrayList<Box>();
+        for (int i = start; i < Math.min(numberOfChilds(),
+                start + columns); i++)
+        {
+            rowChilds.add(childs.get(i));
+        }
+        return rowChilds;
+    }
+
+    public ArrayList<Box> getColumn(int column)
+    {
+        ArrayList<Box> columnChilds = new ArrayList<Box>();
+        for (int i = column; i < numberOfChilds(); i += columns())
+        {
+            columnChilds.add(childs.get(i));
+        }
+        return columnChilds;
     }
 }
--- modules/engine/src/test/java/de/pirckheimer_gymnasium/engine_pi/graphics/boxes/GridBoxTest.java
+++ modules/engine/src/test/java/de/pirckheimer_gymnasium/engine_pi/graphics/boxes/GridBoxTest.java
@@ -8,10 +8,38 @@ import org.junit.jupiter.api.Test;
 
 public class GridBoxTest
 {
+    GridBox gridBox = grid(empty(5));
+
     @Test
-    void numberOfRows()
+    void columns()
     {
-        assertEquals(grid(empty(5)).numberOfRows(), 3);
+        assertEquals(gridBox.columns(), 2);
     }
 
+    @Test
+    void rows()
+    {
+        assertEquals(gridBox.rows(), 3);
+    }
+
+    @Test
+    void getRow()
+    {
+        assertEquals(gridBox.getRow(0).size(), 2);
+        assertEquals(gridBox.getRow(1).size(), 2);
+        assertEquals(gridBox.getRow(2).size(), 1);
+    }
+
+    @Test
+    void getColumn()
+    {
+        assertEquals(gridBox.getColumn(0).size(), 3);
+        assertEquals(gridBox.getColumn(1).size(), 2);
+
+        GridBox g2 = grid(empty(27)).columns(4);
+        assertEquals(g2.getColumn(0).size(), 7);
+        assertEquals(g2.getColumn(1).size(), 7);
+        assertEquals(g2.getColumn(2).size(), 7);
+        assertEquals(g2.getColumn(3).size(), 6);
+    }
 }
