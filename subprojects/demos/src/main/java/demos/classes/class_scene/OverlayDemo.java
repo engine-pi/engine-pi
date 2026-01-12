0839d3424016a0bdc4b9a86d4c0c330f5b808d5e
Josef Friedrich <josef@friedrich.rocks>
2026-01-12 22:16:02 +0100

    Add some copyright notices

--- subprojects/demos/src/main/java/demos/classes/class_scene/OverlayDemo.java
+++ subprojects/demos/src/main/java/demos/classes/class_scene/OverlayDemo.java
@@ -1,3 +1,21 @@
+/*
+ * Engine Pi ist eine anfängerorientierte 2D-Gaming Engine.
+ *
+ * Copyright (c) 2025, 2026 Josef Friedrich and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package demos.classes.class_scene;
 
 import static pi.Controller.colors;
@@ -6,10 +24,11 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 import pi.Circle;
+import pi.Controller;
 import pi.Scene;
 
 /**
- * Demonstriert die Methode {@link Scene#render(Graphics2D, int, int)}.
+ * Demonstriert die Methode {@link Scene#renderOverlay(Graphics2D, int, int)}.
  *
  * <p>
  * Das Demo zeigt zwei grüne Rechtecke, die die Kreisfigur überlagern. Das linke
@@ -26,6 +45,7 @@ public class OverlayDemo extends Scene
     {
         Circle circle = new Circle(15);
         circle.center(0, 0);
+        add(circle);
     }
 
     @Override
@@ -48,7 +68,8 @@ public class OverlayDemo extends Scene
 
     public static void main(String[] args)
     {
-        new OverlayDemo();
+        Controller.instantMode(false);
+        Controller.start(new OverlayDemo());
     }
 
 }
