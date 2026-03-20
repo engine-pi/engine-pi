214e7d1c1300a9e4a3f8deac988b187ceb78bc41
Josef Friedrich <josef@friedrich.rocks>
2026-03-20 20:11:23 +0100

    Test the counter

---
+++ subprojects/engine/src/test/java/pi/actor/CounterTest.java
@@ -0,0 +1,163 @@
+/*
+ * Engine Pi ist eine anfängerorientierte 2D-Gaming Engine.
+ *
+ * Copyright (c) 2026 Josef Friedrich and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package pi.actor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pi.Controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CounterTest
+{
+    private Counter counter;
+
+    @BeforeEach
+    public void setUp()
+    {
+        Controller.instantMode(false);
+        counter = new Counter();
+    }
+
+    @Test
+    public void testCounterInitialization()
+    {
+        assertEquals(0, counter.counter());
+        assertEquals("0", counter.content());
+    }
+
+    @Test
+    public void testCounterWithParameters()
+    {
+        Counter c = new Counter(5, "prefix_", "{counter}_template", "_suffix");
+        assertEquals(5, c.counter());
+        assertEquals("prefix_5_template_suffix", c.content());
+    }
+
+    @Test
+    public void testSetCounter()
+    {
+        counter.counter(10);
+        assertEquals(10, counter.counter());
+        assertEquals("10", counter.content());
+    }
+
+    @Test
+    public void testIncrease()
+    {
+        assertEquals(1, counter.increase());
+        assertEquals(1, counter.counter());
+    }
+
+    @Test
+    public void testIncreaseWithCustomAmount()
+    {
+        counter.amount(5);
+        assertEquals(5, counter.increase());
+        assertEquals(5, counter.counter());
+    }
+
+    @Test
+    public void testDecrease()
+    {
+        counter.counter(10);
+        assertEquals(9, counter.decrease());
+        assertEquals(9, counter.counter());
+    }
+
+    @Test
+    public void testDecreaseWithCustomAmount()
+    {
+        counter.counter(20);
+        counter.amount(3);
+        assertEquals(17, counter.decrease());
+        assertEquals(17, counter.counter());
+    }
+
+    @Test
+    public void testReset()
+    {
+        counter.counter(100);
+        counter.reset();
+        assertEquals(0, counter.counter());
+        assertEquals("0", counter.content());
+    }
+
+    @Test
+    public void testPrefix()
+    {
+        counter.prefix("Zähler: ");
+        counter.updateContent();
+        assertEquals("Zähler: 0", counter.content());
+    }
+
+    @Test
+    public void testSuffix()
+    {
+        counter.suffix(" Punkte");
+        counter.updateContent();
+        assertEquals("0 Punkte", counter.content());
+    }
+
+    @Test
+    public void testTemplate()
+    {
+        counter.template("Score: {counter}");
+        counter.updateContent();
+        assertEquals("Score: 0", counter.content());
+    }
+
+    @Test
+    public void testTemplateWithPrefixAndSuffix()
+    {
+        counter.prefix("[").template("{counter}/10").suffix("]");
+        counter.counter(5);
+        counter.updateContent();
+        assertEquals("[5/10]", counter.content());
+    }
+
+    @Test
+    public void testTemplateWithoutPlaceholder()
+    {
+        counter.template("Invalid");
+        assertThrows(RuntimeException.class, counter::updateContent);
+    }
+
+    @Test
+    public void testAmount()
+    {
+        counter.amount(7);
+        assertEquals(7, counter.amount());
+    }
+
+    @Test
+    public void testGettersAndSetters()
+    {
+        counter.prefix("pre_");
+        counter.suffix("_suf");
+        counter.template("{counter}");
+        counter.amount(3);
+
+        assertEquals("pre_", counter.prefix());
+        assertEquals("_suf", counter.suffix());
+        assertEquals("{counter}", counter.template());
+        assertEquals(3, counter.amount());
+    }
+}
