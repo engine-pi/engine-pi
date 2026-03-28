f7a27d0b30fe834ad4abffb527c4261c68649aa2
Josef Friedrich <josef@friedrich.rocks>
2026-03-28 09:17:24 +0100

    Add copyright to the demos

--- subprojects/demos/src/main/java/demos/classes/dsa/threads/philosophers/package-info.java
+++ subprojects/demos/src/main/java/demos/classes/dsa/threads/philosophers/package-info.java
@@ -1,3 +1,22 @@
+/*
+ * Engine Pi ist eine anfängerorientierte 2D-Gaming Engine.
+ *
+ * Copyright (c) 2026 Josef Friedrich and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * Quelle: <a href=
  * "https://webcodes-prod.obs-website.eu-de.otc.t-systems.com/codes/assetfiles/nepiba/ERBdtU9jKqD8VD7xIIFO0K/K4_Java_Vorl.zip">K4_Java_Vorl.zip</a>
